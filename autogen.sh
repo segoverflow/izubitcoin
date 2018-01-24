d23099f843d75e6288b3137b1d31f864f3cec5a0
reizu <reizu@seguridadoverflow.com>
2018-01-24 23:06:15 +0100

    C++11 mandatory

--- autogen.sh
+++ autogen.sh
@@ -4,4 +4,4 @@ srcdir="$(dirname $0)"
 cd "$srcdir"
 which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf --install --force --warnings=all
+autoreconf --install --force --warnings=all -I m4
